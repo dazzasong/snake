3c859df7be17489da86d4c20189c856bd7f4d870
Darren Song <dazzasong@gmail.com>
2024-12-30 15:24:07 +1100

    Layout complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import { Stack, Typography } from "@mui/material";
+import React from "react";
+import { Button, Stack, Typography } from "@mui/material";
 import Game from "./Game";
 
 function App() {
+  const [gameState, setGameState] = React.useState(0);
+  const [settingsWindowOpen, setSettingsWindowOpen] = React.useState(false);
+
   return (
     <div>
-      <Stack alignItems="center">
+      <Stack alignItems="center" spacing={4}>
         <Typography fontSize={30} fontWeight="bold">Snake</Typography>
+        <Stack
+          direction="row"
+          justifyContent="space-around"
+          alignItems="center"
+          bgcolor="gray"
+          width={600}
+          height={60}
+        >
+          <Button
+            variant="contained"
+            color="success"
+            onClick={() => setGameState(1)}
+          >
+            Start Game
+          </Button>
+          <Button
+            variant="contained"
+            color="warning"
+            onClick={() => setSettingsWindowOpen(true)}
+          >
+            Settings
+          </Button>
+        </Stack>
+        <Game />
       </Stack>
-      <Game />
     </div>
   );
 }
--- src/Game.jsx
+++ src/Game.jsx
@@ -1,9 +1,30 @@
-import { Box } from "@mui/material";
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-function Game() {
+function Base() {
   return (
-    <Box>
-    </Box>
+    <Box width={16} height={16} bgcolor="white" />
+  );
+}
+
+function Player({ points }) {
+  const body = Array.from(Array(points).keys()).map(() =>
+    <Base />
+  );
+
+  return (
+    <Stack spacing={0.5}>
+      {body}
+    </Stack>
+  );
+}
+
+function Game({ state, points }) {
+  return (
+    <Stack justifyContent="center" alignItems="center" bgcolor="black" width={600} height={600}>
+      <Typography color="white">Waiting for game to start...</Typography>
+      <Player />
+    </Stack>
   );
 }
 
